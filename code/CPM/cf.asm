012bd8fb5d80e310bf1642e615fe3fd3792c21fd
Marek Wiecek <marekw1986@gmail.com>
2025-03-13 20:00:48 +0100

    BIOS prepared for read optimization/caching

--- code/CPM/cf.asm
+++ code/CPM/cf.asm
@@ -66,6 +66,56 @@ CFRSECT:
 		CALL CFCHERR
 		RET
         
+; Reads single sector
+; Source in CFLBAx variables
+; Destination address is BLKDAT     
+CFRSECT_WITH_CACHE:
+		LDA CFVAL						; Check if we have valid data in buffer
+		CPI 00H
+		JZ	CFRSECT_WITH_CACHE_PERFORM  ; If not, read
+		LXI H, CFLBA3					; Check if old and new LBA values are equal
+		LXI D, PCFLBA3
+		CALL IS32BIT_EQUAL
+		CPI 00H							; If not, new LBA. Read imediately
+		JZ CFRSECT_WITH_CACHE_PERFORM
+		; We already have valid data in buffer. No need to read it again
+		MVI A, 00H						; Store 0 in A to signalize no err
+		RET
+CFRSECT_WITH_CACHE_PERFORM:
+		CALL CFSLBA						;SET LBA
+		MVI A, 01H
+		OUT	CFREG2						;READ ONE SECTOR
+		CALL CFWAIT
+		MVI A, 20H						;READ SECTOR COMMAND
+		OUT	CFREG7
+		LXI	D, BLKDAT
+		CALL CFREAD
+		CALL CFCHERR
+		CPI 00H							; If A=0, no error, good read
+		JNZ CFRSECT_WITH_CACHE_BAD
+		PUSH PSW
+		PUSH H
+		PUSH D
+		PUSH B
+		MVI A, 01H
+		STA CFVAL
+		; copy CFLBAx toPCFLBAx
+		LXI D, CFLBA3
+		LXI H, PCFLBA3
+		MVI B, 4
+		CALL MEMCOPY
+		POP B
+		POP D
+		POP H
+		POP PSW 
+		RET
+CFRSECT_WITH_CACHE_BAD:
+        PUSH PSW
+        MVI A, 00H
+        STA CFVAL
+        POP PSW
+		RET
+        
 CFWSECT:
         CALL CFSLBA                     ;SET LBA
         MVI A, 01H
